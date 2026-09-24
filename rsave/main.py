08f8da6b26961587937614d8b25a53300a87d990
Suikyou <evaunitsix@gmail.com>
2026-09-24 20:51:17 +0800

    - added date and time for backups and labeling - logic for creating the backup folder in root

--- rsave/main.py
+++ rsave/main.py
@@ -1,13 +1,20 @@
 from pathlib import Path
+from datetime import datetime
 
-
+#variables
 save_directory = Path(input("Enter save directory: ").strip('"'))
+time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+target_dir = Path("backup") / time_stamp
 
+#logic
 if save_directory.is_dir():
-    print("Directory. END")
     for file in save_directory.iterdir():
         print(file.name)
+    print("Directory backup successful", time_stamp)
 elif save_directory.is_file():
-    print("File. END")
+    print("File backup successful", time_stamp)
 else:
     print("Does not exist or invalid input")
+    exit()
+
+target_dir.mkdir(parents=True, exist_ok=True)
