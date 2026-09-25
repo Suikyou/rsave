b4e9554dbfb9d47a417b418131300342c52dbc8f
Suikyou <evaunitsix@gmail.com>
2026-09-25 15:46:44 +0800

    - added try except - added error handling - added rm of directory in case of failure

--- rsave/backup.py
+++ rsave/backup.py
@@ -7,9 +7,17 @@ def backup(save_directory):
     target_dir = Path("backup") / time_stamp
     target_dir_specific = target_dir / save_directory.name
 
-    target_dir.mkdir(parents=True)
+    try:
+        target_dir.mkdir(parents=True)
 
-    if save_directory.is_dir():
-        shutil.copytree(save_directory, target_dir_specific)
-    elif save_directory.is_file():
-        shutil.copy2(save_directory, target_dir)
+        if save_directory.is_dir():
+            shutil.copytree(save_directory, target_dir_specific)
+        elif save_directory.is_file():
+            shutil.copy2(save_directory, target_dir)
+    except OSError as error:
+        if target_dir.exists():
+            shutil.rmtree(target_dir)
+        print("Backup failed.")
+        print(error)
+    else:
+        print(f"Backup successful. Location is at {target_dir}")
